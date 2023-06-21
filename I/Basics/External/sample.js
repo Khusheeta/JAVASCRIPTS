abfaf4d743eeda18f69b849258022865f1b3b6d6
Khusheeta <khusheetaattarde11@gmail.com>
2023-06-21 18:22:53 +0530

    Initial commit

---
+++ I/Basics/External/sample.js
@@ -0,0 +1,11 @@
+
+
+// Js can be written in same file or in separate file as well.
+//If separate file- How to use that?
+//1. If the code is in same file with html, the file looks messy beacuse js itself might have 3k-4k line of codes. (for neatness*)
+//2. To load the webpage faster- webpage is of two parts .html tags & .css or js if we put every code in same file the amount of time tooo transfer would be more.hence maintain separate file for js and load the html file first and then css or js.(for faster loading*)
+
+var first = function(){
+    console.log('hello world111');
+    alert('hello world111');
+};
